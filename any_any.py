179797a0d8d50101273848be33a25ed4e4164abe
Subekshya Kayastha <subekshyakayastha4@gmail.com>
2022-08-13 17:42:55 +0545

    any_any

--- any_any.py
+++ any_any.py
@@ -1,7 +1,12 @@
-# --> It represents a one line comment in python
+# --> Any_any
 
 '''
-It can represent a multi-line comment
-We can write any thing here 
+My name is Subekshya Kayastha. I am studying computing right now.
+I have interests other than coding or programming as well.
+Some of them are sports like swimming and basketball.
+I also like drawing as a hobby.
+I like to draw landscapes and anime characters.
+I mostly spend my free time watching dramas or series.
+
 '''
 
